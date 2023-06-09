1d64a334c12b1a93980d8bbbabca06b22ef3309a
Kasra-Z-A <38665356+Kasra-Z-A@users.noreply.github.com>
2023-06-09 18:02:30 +0200

    added python file for job description embeddings

---
+++ job_description_embedding/notebooks/JobMatching.py
@@ -0,0 +1,99 @@
+import os
+import json
+import xml.etree.ElementTree as ET
+import faiss
+import pickle
+import numpy as np
+from langchain.embeddings import HuggingFaceEmbeddings
+from dotenv import load_dotenv
+
+class JobMatching:
+
+    def __init__(self, embeddings: HuggingFaceEmbeddings):
+        self.embeddings = embeddings
+        load_dotenv()  # Load environment variables from .env file
+
+    def parse_xml(self, xml_file):
+        tree = ET.parse(xml_file)
+        root = tree.getroot()
+
+        jobs_list = []
+        for job in root.findall('job'):
+            job_dict = {
+            "title": job.find('title').text,
+            "company": job.find('company').text,
+            "posted_date": job.find('posted_date').text,
+            "job_reference": job.find('job_reference').text,
+            "req_number": job.find('req_number').text,
+            "url": job.find('url').text,
+            "body": job.find('body').text,
+            "city": job.find('city').text,
+            "state": job.find('state').text,
+            "country": job.find('country').text,
+            "location": job.find('location').text,
+            "function": job.find('function').text,
+            "logo": job.find('logo').text,
+            "jobtype": job.find('jobtype').text,
+            "education": job.find('education').text,
+            "experience": job.find('experience').text,
+            "salary": job.find('salary').text,
+            "requiredlanguages": job.find('requiredlanguages').text,
+            "requiredskills": job.find('requiredskills').text
+        }
+            jobs_list.append(job_dict)
+
+        return jobs_list
+
+    def xml_to_json(self, xml_file, json_output_file):
+        jobs_list = self.parse_xml(xml_file)
+        json_output = json.dumps(jobs_list, indent=4)
+
+        with open(json_output_file, 'w') as json_file:
+            json_file.write(json_output)
+
+    def create_embeddings(self, json_file):
+        with open(json_file, 'r') as f:
+            data = json.load(f)
+
+        strings = []
+        for obj in data:
+            string = json.dumps(obj)
+            strings.append(string)
+
+        doc_result = self.embeddings.embed_documents(strings)
+
+        index = faiss.index_factory(len(doc_result[0]), "Flat")
+        index.train(doc_result)
+        index.add(doc_result)
+
+        return index, strings
+
+    def match_jobs(self, index, strings, query, k=5):
+        query_result = self.embeddings.embed_query(query)
+        distances, neighbors = index.search(query_result.reshape(1,-1).astype(np.float32), k)
+
+        for neighbor in neighbors[0]:
+            print(strings[neighbor])
+
+    def save_embeddings(
+        self,
+        embeddings,
+        saving_embeddings_file_name: str = os.getenv("SAVING_EMBEDDINGS_FILE_NAME"),
+        saving_embeddings_directory: str = os.getenv("SAVING_EMBEDDINGS_DIRECTORY"),
+    ) -> None:
+
+        directory = os.path.join(os.getcwd(), saving_embeddings_directory)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        file_path = os.path.join(directory, saving_embeddings_file_name + ".pkl")
+
+        # Save embeddings to binary file
+        with open(file_path, "wb") as f:
+            pickle.dump(embeddings, f)
+
+    def load_embeddings(self, embeddings_path) -> HuggingFaceEmbeddings:
+
+        with open(embeddings_path, "rb") as f:
+            embeddings: HuggingFaceEmbeddings = pickle.load(f)
+
+        return embeddings
